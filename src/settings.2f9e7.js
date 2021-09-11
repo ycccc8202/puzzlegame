333b56e5864c4e99828d5ffe80ae1d658bf95ed2
yechun <21882889@qq.com>
2021-09-12 05:47:13 +0800

    更新

---
+++ src/settings.2f9e7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"468ae",resources:"55d0e",main:"c082a"}};
